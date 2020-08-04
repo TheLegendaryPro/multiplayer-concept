3329a959c20bfe77a2e6bffe6b6b420b93fe08eb
BuildTools <unconfigured@null.spigotmc.org>
2020-08-04 21:57:52 +0800

    Added a website to warp it

--- public/index.js
+++ public/index.js
@@ -5,8 +5,8 @@ import Client from "./js/client.js"
 // The config to run game on
 var config = {
     type: Phaser.AUTO,
-    width: 800,
-    height: 600,
+    width: window.innerWidth * 0.6,//800,
+    height: window.innerHeight * 0.6,//600,
     title: "ust.io",
     physics: {
         default: 'arcade',
@@ -15,6 +15,8 @@ var config = {
             debug: true
         }
     },
+    zoom: 1.3,
+    parent: "game",
     scene: [main, atrium]
 }
 
